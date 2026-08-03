4c27b12f71b1c63a97031381d5789401f8e272a4
Juratbek Otaboyev <jotaboyev0717@gmail.com>
2026-08-03 17:58:33 +0500

    uncomplete one.txt ni yangiladim

--- 51-100/98.py
+++ 51-100/98.py
@@ -1,10 +1,11 @@
-a = input()
-b = input()
+a = list(input())
+b = list(input())
 s = 0
 for i in a:
     if i in b:
+        b.remove(i)
         s += 1
-if len(a) == len(b) == s:
+if len(a) == s:
     print(True)
 else:
     print(False)
